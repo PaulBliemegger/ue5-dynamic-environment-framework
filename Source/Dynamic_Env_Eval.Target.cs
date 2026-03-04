fee88b087dba03b42e6068b3cbe3db017f914ec7
Paul Bliemegger <pbliemegger@gmail.com>
2026-03-04 12:11:14 +0100

    Upgraded to 5.7.3

--- Source/Dynamic_Env_Eval.Target.cs
+++ Source/Dynamic_Env_Eval.Target.cs
@@ -8,8 +8,8 @@ public class Dynamic_Env_EvalTarget : TargetRules
 	public Dynamic_Env_EvalTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("Dynamic_Env_Eval");
 	}
 }
--- Source/Dynamic_Env_EvalEditor.Target.cs
+++ Source/Dynamic_Env_EvalEditor.Target.cs
@@ -8,8 +8,8 @@ public class Dynamic_Env_EvalEditorTarget : TargetRules
 	public Dynamic_Env_EvalEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("Dynamic_Env_Eval");
 	}
 }
