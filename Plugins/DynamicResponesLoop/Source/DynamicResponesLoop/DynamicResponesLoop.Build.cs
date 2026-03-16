883a2c569097b3ae42259e90f9df717118d540b4
Paul Bliemegger <pbliemegger@gmail.com>
2026-03-16 10:49:02 +0100

    add dependencies

--- Plugins/DynamicResponesLoop/Source/DynamicResponesLoop/DynamicResponesLoop.Build.cs
+++ Plugins/DynamicResponesLoop/Source/DynamicResponesLoop/DynamicResponesLoop.Build.cs
@@ -27,6 +27,7 @@ public class DynamicResponesLoop : ModuleRules
 			{
 				"Core",
 				// ... add other public dependencies that you statically link with here ...
+				"GameplayTags",
 			}
 			);
 			
